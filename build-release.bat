34403c05db5f3879902d016552d01d2ed3ba957f
git-krytoi-user <165947403+git-krytoi-user@users.noreply.github.com>
2024-06-02 11:44:17 +0300

    Add files via upload

---
+++ build-release.bat
@@ -0,0 +1,18 @@
+@ECHO off
+SETLOCAL EnableExtensions
+SETLOCAL EnableDelayedExpansion
+CD /d "%~dp0"
+SET DIR=%~dp0
+
+REM
+FOR /F "tokens=1,2,3 delims=/ " %%a IN ('DATE/T') DO SET CURRENT_DATE=%%c%%a%%b
+
+REM
+MKDIR .releases >nul 2>&1
+
+REM
+"%cd%\.build\pawncc.exe" -;+ -(+ -w=217 -v=2 -d=3 -i="%cd%\.build\include" "%cd%\.sources\new.pwn"
+MKDIR ".releases\%CURRENT_DATE% build" >nul 2>&1
+MOVE /Y new.amx ".releases\%CURRENT_DATE% build\new.amx" >nul 2>&1
+
+timeout /t 2
